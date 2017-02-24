91a850c2a41ccebcb27ff1b414693a29e34f1ea5
watanabear <ry0.watana6e@gmail.com>
2017-02-25 00:12:56 +0900

    Tweet取得

--- MDBLibHandson/app/src/main/java/com/example/watanabear/mdblibhandson/TimeLineFragment.java
+++ MDBLibHandson/app/src/main/java/com/example/watanabear/mdblibhandson/TimeLineFragment.java
@@ -3,6 +3,7 @@ package com.example.watanabear.mdblibhandson;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -10,43 +11,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.watanabear.mdblibhandson.model.Tweet;
+import com.example.watanabear.mdblibhandson.repository.TweetRealmRepository;
+import com.example.watanabear.mdblibhandson.repository.TweetRepository;
 
 import java.util.Arrays;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TimeLineFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TimeLineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TimeLineFragment extends ListFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public TimeLineFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimeLineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TimeLineFragment newInstance(String param1, String param2) {
         TimeLineFragment fragment = new TimeLineFragment();
         Bundle args = new Bundle();
@@ -63,77 +47,36 @@ public class TimeLineFragment extends ListFragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        final ListAdapter adapter = new ArrayAdapter<>(getContext(),
-                R.layout.list_item_tweet,
-                R.id.text,
-                Arrays.asList("Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                ));
-
-        setListAdapter(adapter);
     }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_line, container, false);
-    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        TweetRepository repository = new TweetRealmRepository();
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        final ArrayAdapter<Tweet> adapter = new ArrayAdapter<Tweet>(
+                getContext(),R.layout.list_item_tweet){
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final Tweet tweet = getItem(position);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.list_item_tweet, parent, false);
+                }
+                ((TextView) convertView.findViewById(R.id.screen_name)).setText(tweet.getScreenName());
+                ((TextView) convertView.findViewById(R.id.text)).setText(tweet.getText());
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+                return convertView;
+            }
+        };
+        repository.loadTimeLine().subscribe(result -> {
+            setListAdapter(adapter);
+        }, throwable -> {
+            Toast.makeText(getContext(), "error", Toast.LENGTH_LONG).show();
+        });
     }
 }
